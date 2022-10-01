f541e0aafb5de8efbebc7202c95903b82f5008a4
glebe <glebe@BELG04>
2022-10-01 12:09:48 +0200

    Добавьте файлы проекта.

---
+++ lab1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Лукьянов Глеб   22-ИСП2\1   Номер:2   Уровнь:Высокий
+Console.WriteLine("Ведите число x:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Ведите число y:");
+double y = double.Parse(Console.ReadLine());
+
+double p =Math.Sqrt(x*x + y*y);
+double f = f = y / x;
+
+
+Console.WriteLine(p);
+Console.WriteLine(f) ;
